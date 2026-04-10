7e79cd202ea2880bf0e0604214f04678f2445d5b
azhrielbaik <azhrielmuari@gmail.com>
2026-04-10 11:25:55 +0700

    Landing Page

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,19 @@
-import './bootstrap';
+// resources/js/app.js
+import "./bootstrap";
+
+// Mendaftarkan komponen Alpine.js untuk mengatur state header
+document.addEventListener("alpine:init", () => {
+    Alpine.data("delimaCareLayout", () => ({
+        scrolled: false,
+
+        init() {
+            // Cek posisi scroll saat pertama kali dimuat
+            this.scrolled = window.pageYOffset > 20;
+
+            // Pantau pergerakan scroll
+            window.addEventListener("scroll", () => {
+                this.scrolled = window.pageYOffset > 20;
+            });
+        },
+    }));
+});
---
+++ resources/views/components/feature-card.blade.php
@@ -0,0 +1,7 @@
+<div class="p-8 border border-gray-200 rounded-xl hover:shadow-lg transition">
+    <div class="w-12 h-12 mb-6 text-gray-800">
+        {{ $icon }}
+    </div>
+    <h3 class="text-xl font-semibold mb-3">{{ $title }}</h3>
+    <p class="text-gray-600 text-sm">{{ $description }}</p>
+</div>
