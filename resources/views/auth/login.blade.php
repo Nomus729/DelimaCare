b5266cc673c302474ea26fc42161c8f99c16d457
azhrielbaik <azhrielmuari@gmail.com>
2026-04-10 10:41:46 +0700

    login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,14 +1,17 @@
-<form action="/login" method="POST">
-    @csrf
+<form action="/login" method="POST"> @csrf
+
     <div>
         <label>Email</label>
         <input type="email" name="email" value="{{ old('email') }}" required>
-        @error('email') <small>{{ $message }}</small> @error
-    </div>
+        @error('email')
+            <small>{{ $message }}</small>
+        @enderror </div>
 
     <div>
         <label>Password</label>
         <input type="password" name="password" required>
+        @error('password') <small>{{ $message }}</small>
+        @enderror
     </div>
 
     <button type="submit">Login</button>
