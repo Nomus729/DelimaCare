feb497871a3ede4837bde7485e03b85b74ff95d9
Nomus729 <skyh1023@gmail.com>
2026-05-04 10:41:41 +0700

    [Add] Okee

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         $schedule->command('reservasi:update-status')->daily();
-        $schedule->command('sync:remote-to-local')->everyFiveMinutes();
+        $schedule->command('sync:remote-to-local')->everyMinute();
     }
 
     /**
